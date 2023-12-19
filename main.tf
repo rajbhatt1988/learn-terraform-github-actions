22550be49f30b39b3eabb87f2424612f5434b43d
rajbhatt1988 <153775729+rajbhatt1988@users.noreply.github.com>
2023-12-19 14:45:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-/*resource "aws_vpc" "My_VPC" {
+resource "aws_vpc" "My_VPC" {
   cidr_block = "10.11.0.0/16"
 tags = {
 Name = "My_VPC"
 }
-}*/
-/*resource "aws_subnet" "aws_subnet" {
+}
+resource "aws_subnet" "MySubnet" {
   vpc_id     = aws_vpc.My_VPC.id
   cidr_block = "10.11.1.0/24"
   tags = {
     Name = "Mysubnet"
   }
-}*/
+}
