2b8c851573afa6d1aec19aab8a690dcb2a8830b5
rajbhatt1988 <153775729+rajbhatt1988@users.noreply.github.com>
2023-12-19 13:22:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.11.0.0/16"
 tags = {
 Name = "My_VPC"
+}
+resource "aws_subnet "testsubnet" {
+cidr_block = "10.11.1.0/24"
+tags = {
+Name = "MyTestSubnet"
 }
 }
