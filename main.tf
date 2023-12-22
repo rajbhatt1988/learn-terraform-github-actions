c071b629043382e5ecefe6fe7cfd365ca32b579f
rajbhatt1988 <153775729+rajbhatt1988@users.noreply.github.com>
2023-12-22 09:48:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
-# main.tf
-
-provider "aws" {
-  region = "us-east-1"
+resource "aws_vpc" "My_VPC" {
+  cidr_block = "10.11.0.0/16"
+tags = {
+Name = "My_VPC"
 }
-
-module "iam" {
-  source    = "./modules/iam"
-  user_name = "testuser"
+}
+resource "aws_subnet" "MySubnet" {
+  vpc_id = "aws_vpc.My_VPC.id"
+  cidr_block = "10.11.1.0/24"
+  tags = {
+    Name = "Mysubnet"
+  }
 }
