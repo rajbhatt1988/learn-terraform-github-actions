f444fb49390d0497ecedf65ad520e49370d774a8
rajbhatt1988 <153775729+rajbhatt1988@users.noreply.github.com>
2023-12-22 09:48:02 -0500

    Update main.tf

--- modules/IAM/main.tf
+++ modules/IAM/main.tf
@@ -1,11 +1,11 @@
 # modules/iam/main.tf
 
-variable "user_name" {
+/*variable "user_name" {
   type        = string
   description = "testuser"
 }
 
 resource "aws_iam_user" "iam_user" {
   name = var.user_name
-}
+}*/
 
